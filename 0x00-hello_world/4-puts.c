1b4b6a715f82d101b8357c57ac6ece745f2c0055
andaleeb55 <andaleephelal@gmail.com>
2023-02-17 10:24:29 -0800

    task 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
  * main -print a line of code using puts 
  *
- * Retutn: 0
-*/
-int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+ * Retutn:0 
+ *\
+ int main(void)
+{
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }	
