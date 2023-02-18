b4d8cee6ea52edd29b51c6c3da12f51f4ec645c3
andaleeb55 <andaleephelal@gmail.com>
2023-02-18 01:51:38 -0800

    task 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - A program that prints a line with puts functio
 n
  * Retutn: 0 (success)
-*\
+ *\
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-{
+}
