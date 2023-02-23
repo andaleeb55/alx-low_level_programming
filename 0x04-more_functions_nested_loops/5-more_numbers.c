f3ad96da365bd8c0e3fc51009c47b1e5b614119d
andaleeb55 <andaleephelal@gmail.com>
2023-02-23 14:50:22 -0800

    task 2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,26 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * more_numbers - function to print numbers
- *
- * Return: Always 0.
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: Can only use _putchar 3 times
  */
 void more_numbers(void)
 {
-	int a, b;
+	int c, i;
 
-	for (a = 0; a < 10; a++)
+	c = 0;
+	while (c < 10)
 	{
-		for (b = 0; b < 15; b++)
+		i = 0;
+		while (i < 15)
 		{
-			if (b > 9)
-			_putchar(b / 10 + '0');
-			_putchar(b % 10 + '0');
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+			i++;
 		}
+		c++;
 		_putchar('\n');
 	}
+}
