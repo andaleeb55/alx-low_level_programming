e742327d9ca42912a691fce28079fdbf8fc8b090
andaleeb55 <andaleephelal@gmail.com>
2023-03-21 06:00:52 -0700

    add project `0x0C-more_malloc_free`

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory of size @b else exit
+ * @b: the size of the memory to allocate
+ * Description:
+ * allocates memory of size @b and returns address points to the memory
+ * otherwise exits with status code 98
+ *
+ * Return: the address of the allocated memory of success
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (!ptr)
+		exit(98);
+	return (ptr);
+}
